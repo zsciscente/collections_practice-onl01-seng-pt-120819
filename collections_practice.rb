8ff188e9a50143ececae3743eb16594ab8805d4d
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 19:35:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(n)
+  n.sort
+end
 
+def sort_array_desc(n)
+  n.sort.reverse
+end
+
+def sort_array_char_count(n)
+  n.sort do |a, b|
+    a.length <=> b.length
+end
+  
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(n)
+  n.reverse
+end
+
+def kesha_maker(n)
+  n.each {|word|
+    word[2] = "$"
+  }
+end
+
+def find_a(arguement)
+ na = []
+ arguement.each {|name|
+  if name.start_with?("a")
+    na << "#{name}"
+  end}
+  na
+end
+
+def sum_array(n)
+  n.inject { |sum, i| sum + i }
+end  
+
+def add_s(n)
+  n.collect {|word|
+  if word == word[1]
+    return word
+  else
+  word << "s"
+end
+  }
+end
